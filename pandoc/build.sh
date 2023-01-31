afa02bbbc56d6e2ebffb81da7c973c5f047da69f
qili <lq3141@gmail.com>
2023-01-31 11:52:25 +0800

    refactor

--- pandoc/build.sh
+++ pandoc/build.sh
@@ -1,7 +1,23 @@
+#!/bin/bash
+download_en=false
 
 # pandoc 3.0
-wget https://github.com/jgm/pandoc/releases/download/3.0/pandoc-3.0-linux-amd64.tar.gz
-wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.15.0/pandoc-crossref-Linux.tar.xz
+if [ "$download_en" = true ]; then
+    wget https://github.com/jgm/pandoc/releases/download/3.0/pandoc-3.0-linux-amd64.tar.gz
+    wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.15.0/pandoc-crossref-Linux.tar.xz
+fi
+
+tar zxf pandoc-3.0-linux-amd64.tar.gz
+mv pandoc-3.0 pandoc
+cd pandoc/bin
+tar Jxf ../../pandoc-crossref-Linux.tar.xz
+cd -
+
+# plantuml
+if [ "$download_en" = true ]; then
+    wget https://github.com/plantuml/plantuml/releases/download/v1.2023.0/plantuml-1.2023.0.jar
+fi
+cp plantuml-1.2023.0.jar plantuml.jar
 
 # docker image
-docker build -t pandoc:v1 .
+#sudo docker build -t pandoc:v1 .
