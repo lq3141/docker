ea33308921f145bf1b899c68836ea5920cf8bf30
qili <lq3141@gmail.com>
2023-03-16 17:02:52 +0800

    Update jenkins.sh

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,4 +1,7 @@
 
+# bind mount is not good, because of permission error
 sudo chown -R 1000:1000 /home/sysadmin/Docker/jenkins
 docker run -p 8080:8080 -p 50000:50000 -v /home/jenkins/Docker/jenkins:/var/jenkins_home jenkins/jenkins:lts
 
+# docker volume is much better
+docker run -p 8080:8080 -p 50000:50000 --restart=on-failure -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts-jdk11
